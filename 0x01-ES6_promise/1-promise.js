67ec56d80b32673c3a8b91312e46de7f03bdb4e9
kalu_coder <awesomecoding51@gmail.com>
2023-03-22 20:57:19 +0300

    promise with parameters

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+export default function getFullResponseFromAPI(success)
+{
+    return new Promise((resolve, reject) => {
+        if (success)
+        {
+            resolve({
+                status:200,
+                body: success,
+            });
+        }
+        else
+        {
+            reject(new Error("The fake API is not working currently"));
+        }
+    });
+}
