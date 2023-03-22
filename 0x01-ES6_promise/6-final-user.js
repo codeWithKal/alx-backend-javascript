cc4b0ea15f6c67382432d6c2f109dda5f38a276f
kalu_coder <awesomecoding51@gmail.com>
2023-03-22 22:54:06 +0300

    code edited

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,8 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
-export default handleProfileSignup(firstName, lastName, fileName)
-{
-    return Promise
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
       res.map((o) => ({
