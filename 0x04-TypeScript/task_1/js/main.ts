c3ff07983de738ae8d6da9cb68ecfa1c2274bf1e
kalu_coder <kaleabshiferaw51@gmail.com>
2023-03-30 05:40:02 +0300

    interface and function to the print the name of teacher in speciric format

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,3 +7,15 @@ export interface Teacher
     location : string;
     [key : string] : any;    
 }
+
+export interface Directors extends Teacher{
+    numberOfReports : number;
+}
+
+export interface printTeacherFunction{
+    (firstName : string, lastName : string) : string;
+}
+
+export function printTeacher(firstName : string, lastName : string) : string {
+    return `${firstName[0]}. ${lastName}`;
+}
