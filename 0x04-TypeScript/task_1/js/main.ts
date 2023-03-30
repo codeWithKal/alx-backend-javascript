686a92ac2cb1c262a9d72e689b92d27a24da1e63
kalu_coder <kaleabshiferaw51@gmail.com>
2023-03-30 10:21:32 +0300

    class defined with constructors and interfaces to declare constructor and function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,26 @@ export interface printTeacherFunction{
 export function printTeacher(firstName : string, lastName : string) : string {
     return `${firstName[0]}. ${lastName}`;
 }
+export interface IStudentClass{
+    workOnHomework() : string;
+    displayName(): string;
+}
+export interface IStudentClassConstructor{
+    new (firstName: string, lastName : string): IStudentClass;
+}
+export class StudentClass implements IStudentClass{
+    private _firstName!: string;
+    private _lastName!: string;
+
+    constructor(firstName: string, lastName : string){
+    this._firstName = firstName;
+    this._lastName = lastName;
+    }
+    workOnHomework(): string {
+        return "currently working";
+    }
+    displayName(): string {
+        return this._firstName;
+    }
+
+}
