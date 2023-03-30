150bf04b25dacacf562068f1757cce379332588c
kalu_coder <kaleabshiferaw51@gmail.com>
2023-03-30 05:18:57 +0300

    fat arrow function

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,26 +1,28 @@
-interface Student{
+export interface Student{
     firstName : string;
     lastName : string;
     age : number;
     location :string;
 }
-const studentsList : Student[] = [
+const studentsList : Array<Student> = [
     {firstName : "kaleab", lastName : "shiferaw", age : 23, location : "Aleltu"},
     {firstName : "mihiretu", lastName : "Endeshaw", age : 23, location: "Debrebirihan"}
 ]
 
-const table_body = document.querySelector("tbody");
-for(let i = 0; i <= studentsList.length; i++)
-{
-    const row = document.createElement("tr");
-    const firstName = document.createElement("td");
-    const Location = document.createElement("td");
-
-
-    firstName.textContent = studentsList[i].firstName;
-    Location.textContent = studentsList[i].location;
-
-    row.appendChild(firstName);
-    row.appendChild(Location);
-    table_body.appendChild(row);
+export const DisplayTable = ( studentsList : Array<Student>) => {
+    const table_body = document.querySelector("tbody");
+    for(let i = 0; i <= studentsList.length; i++)
+    {
+        const row = document.createElement("tr");
+        const firstName = document.createElement("td");
+        const Location = document.createElement("td");
+    
+    
+        firstName.textContent = studentsList[i].firstName;
+        Location.textContent = studentsList[i].location;
+    
+        row.appendChild(firstName);
+        row.appendChild(Location);
+        table_body.appendChild(row);
+    }
 }
